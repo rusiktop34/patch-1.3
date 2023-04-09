55aab2793852b2ad0fcedabbfa1f3f433d13770a
rusiktop34 <privetiki223@gmail.com>
2023-04-09 12:56:36 +0300

    1.3

---
+++ MapDungeon1/Assets/script/IDLEBEHAIDER.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IDLEBEHAIDER : StateMachineBehaviour
+{
+    float timer;
+   
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = 0;
+    }
+
+    
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer += Time.deltaTime;
+        if (timer > 5)
+            animator.SeTBool("isPatrulling", true);
+    }
+
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+   
+}
